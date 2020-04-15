a7449d9c15aa4f2ea513caaaae6c3e9a38153779
Anand Balasubramanian <bala.anand@gmail.com>
2020-04-15 17:34:24 -0500

    Update constants.js

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,2 +1,2 @@
 export const SHEETS_URL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vR_-5Gt7fsmpyM3M9m315bFUFv7jkt9c3Tu6Qg7xzCbVeGJlUlKjLsKTLOBxNFRh9EWsD0aRH79hUCw/pub?gid=1902887224&single=true&output=csv";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCJGFerPUmVP8HNdOFsFpur1AT63f2wzsd8gBvKwhbI5zIks2m5inSoKBv_wDl7bYgJ1EbWTcRLHQZ/pub?output=csv";
