8236b3aa11dc46849bc6f7d7be026bd62dfe293a
Anand Balasubramanian <bala.anand@gmail.com>
2020-04-20 11:01:11 -0500

    adding google analytics code

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-162016159-1",
+        trackingId: "UA-385726-9",
         head: false,
         anonymize: true,
         respectDNT: true,
